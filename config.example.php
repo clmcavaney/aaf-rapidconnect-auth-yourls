9bd3595c5425df5937d85b911d6b61904ac52749
Christopher McAvaney <christopher.mcavaney@gmail.com>
2020-04-30 09:09:18 +0000

    cleaned up options

--- config.example.php
+++ config.example.php
@@ -5,15 +5,14 @@ define('AAF_RAPIDCONNECT_KEY', '<your KEY which is generated when you register a
 // https://rapid.test.aaf.edu.au/jwt/authnrequest/research/<hash value>
 define('AAF_RAPIDCONNECT_AUTH_URL', 'https://rapid.aaf.edu.au/jwt/authnrequest/<provided when you register your service>');
 define('AAF_RAPIDCONNECT_ISSUER', 'https://rapid.aaf.edu.au');
+define('AAF_RAPIDCONNECT_AUDIENCE', '<URL provided when you register your service>');
 
 
 // DEV
 // define('AAF_RAPIDCONNECT_KEY', '<your KEY which is generated when you register an app with AAF Rapid Connect>');
 // define('AAF_RAPIDCONNECT_AUTH_URL', 'https://rapid.test.aaf.edu.au/jwt/authnrequest/<provided when you register your service>');
 // define('AAF_RAPIDCONNECT_ISSUER', 'https://rapid.test.aaf.edu.au');
-
-
-define('AAF_RAPIDCONNECT_AUDIENCE', '<URL provided when you register your service>');
+// define('AAF_RAPIDCONNECT_AUDIENCE', '<URL provided when you register your service>');
 
 // Attributes for Rapid Connect are shown on this page: https://rapid.aaf.edu.au/developers
 define('AAF_ATTRIBUTE_USER', 'mail');
