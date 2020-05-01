90095a1b7041f7aa5e82d8287b9199b0ce6f778c
Christopher McAvaney <christopher.mcavaney@gmail.com>
2020-05-01 07:09:17 +0000

    changed the method to use variables in the database rather than config.php defined variables

--- plugin.php
+++ plugin.php
@@ -11,8 +11,6 @@ Author URI: http://deakinresear.ch/eresearch
 // No direct call
 if( !defined( 'YOURLS_ABSPATH' ) ) die();
 
-require_once __DIR__.'/config.php';
-
 /* The function yourls_is_valid_user() in includes/functions-auth.php checks for a valid user via the login 
  * form or stored cookie. The 'shunt_is_valid_user' filter allows plugins such as this one, to short-circuit
  * the entire function.
@@ -67,10 +65,16 @@ function aaf_rapidconnect_auth() {
 		return $unfiltered_valid;
 	}
 
+	$aaf_rapidconnect_key = yourls_get_option( 'aaf_rapidconnect_key' );
+	$aaf_rapidconnect_auth_url = yourls_get_option( 'aaf_rapidconnect_auth_url' );
+	$aaf_rapidconnect_issuer = yourls_get_option( 'aaf_rapidconnect_issuer' );
+	$aaf_rapidconnect_audience = yourls_get_option( 'aaf_rapidconnect_audience' );
+	$aaf_attribute_user = yourls_get_option( 'aaf_attribute_user' );
+
 	if (isset($_SESSION['sub']) && $_SESSION['sub']) {
 		// User has already authenticated against AAF, nothing to do
 		// Just have to reset the username from the session data each time
-		yourls_set_user($_SESSION['attributes'][AAF_ATTRIBUTE_USER]);
+		yourls_set_user($_SESSION['attributes'][$aaf_attribute_user]);
 		if ( !yourls_is_API() ) {
 			// Satisfy yourls' cookie generation routine
 			yourls_store_cookie( YOURLS_USER );
@@ -87,10 +91,10 @@ function aaf_rapidconnect_auth() {
 
 			$config = array();
 			$config['aaf_rapidconnect'] = array(
-											'iss' => AAF_RAPIDCONNECT_ISSUER,
-											'aud' => AAF_RAPIDCONNECT_AUDIENCE
+											'iss' => $aaf_rapidconnect_issuer,
+											'aud' => $aaf_rapidconnect_audience
 											);
-			$key = AAF_RAPIDCONNECT_KEY;
+			$key = $aaf_rapidconnect_key;
 
 			$JWT = new JWT;
 			$verified_jwt = $JWT->decode($_POST['assertion'], $key);
@@ -119,7 +123,7 @@ function aaf_rapidconnect_auth() {
 				$_SESSION['sub'] = $jwt['sub'];
 				$_SESSION['attributes'] = $jwt['https://aaf.edu.au/attributes'];
 
-				yourls_debug_log( "mail attribute == " . $_SESSION['attributes'][AAF_ATTRIBUTE_USER] );
+				yourls_debug_log( "mail attribute == " . $_SESSION['attributes'][$aaf_attribute_user] );
 
 				// At this point we want to validate the user is able to login
 				// Validate against ?? (a list ? - TBC)
@@ -130,7 +134,7 @@ function aaf_rapidconnect_auth() {
 				}
 
 				// get the mail attribute for the user in YOURLS
-				yourls_set_user($_SESSION['attributes'][AAF_ATTRIBUTE_USER]);
+				yourls_set_user($_SESSION['attributes'][$aaf_attribute_user]);
 				if ( !yourls_is_API() ) {
 					// Satisfy yourls' cookie generation routine
 					yourls_store_cookie( YOURLS_USER );
@@ -162,6 +166,8 @@ function aaf_rapidconnect_check_user( $aaf_attributes ) {
 
 // Display the access denied in a nice window
 function aaf_rapidconnect_access_denied( $aaf_attributes ) {
+	$aaf_attribute_user = yourls_get_option( 'aaf_attribute_user' );
+
 	require_once( YOURLS_INC.'/functions-html.php' );
 
 	yourls_html_head( 'access', 'Access denied' );
@@ -169,7 +175,7 @@ function aaf_rapidconnect_access_denied( $aaf_attributes ) {
 
 	echo <<<HTML
 	<h2>Admin access denied</h2>
-	<p>You ({$aaf_attributes['cn']} - {$aaf_attributes[AAF_ATTRIBUTE_USER]}) are not authorised to access the admin pages of this YOURLS instance.</p>
+	<p>You ({$aaf_attributes['cn']} - {$aaf_attributes[$aaf_attribute_user]}) are not authorised to access the admin pages of this YOURLS instance.</p>
 
 	<p>If you believe you should have access, contact <a href="mailto:eresearch@deakin.edu.au?Subject=eresear.ch%20shorten%20URL%20service">Christopher McAvaney</a> for assistance.</p>
 HTML;
@@ -179,6 +185,8 @@ HTML;
 
 // Display simple page with an AAF Login button
 function aaf_rapidconnect_login_screen() {
+	$aaf_rapidconnect_auth_url = yourls_get_option( 'aaf_rapidconnect_auth_url' );
+
 	require_once( YOURLS_INC.'/functions-html.php' );
 	yourls_html_head( 'access', 'AAF Rapid Connect login' );
 	yourls_html_logo();
@@ -186,7 +194,7 @@ function aaf_rapidconnect_login_screen() {
 ?>
 	<div id="login" style="margin-top: 100px;">
 	<p class="error"><?php echo yourls__( 'Please log in' ); ?></p>
-	<form action="<?php echo filter_var(AAF_RAPIDCONNECT_AUTH_URL, FILTER_SANITIZE_URL); ?>" method="GET">
+	<form action="<?php echo filter_var($aaf_rapidconnect_auth_url, FILTER_SANITIZE_URL); ?>" method="GET">
 		<input type="submit" id="launch-login-button" name="launch-login-button" value="<?php yourls_e( 'AAF login' ); ?>" class="button" /></div>
 	</form>
 	<p><a href="http://www.aaf.edu.au"><img src="https://rapid.aaf.edu.au/aaf_service_110x26.png" /></a></p>
@@ -208,4 +216,99 @@ function aaf_rapidconnect_logout() {
 
 	yourls_debug_log( "aaf_rapidconnect_logout() should now be logged out" );
 }
+
+// Add to the admin page
+yourls_add_action( 'html_head', 'aaf_rapidconnect_headers' );
+
+function aaf_rapidconnect_headers() {
+	echo <<<HTML
+	<style type="text/css">
+		label {
+			padding-right: 0.25rem;
+			width: 10rem;
+			display: inline-block;
+			text-align: right;
+		}
+	</style>
+
+HTML;
+}
+
+yourls_add_action( 'plugins_loaded', 'aaf_rapidconnect_add_page' );
+
+function aaf_rapidconnect_add_page() {
+	yourls_register_plugin_page( 'aaf_rapidconnect', 'AAF Rapid Connect', 'aaf_rapidconnect_do_page' );
+}
+
+// Display the plugin page
+function aaf_rapidconnect_do_page() {
+	// Check if a form was submitted
+	if ( isset( $_POST['aaf_rapidconnect_key'] ) ) {
+		// check nonce
+		yourls_verify_nonce( 'aaf_rapidconnect' );
+
+		// process form
+		yourls_update_option( 'aaf_rapidconnect_key', $_POST['aaf_rapidconnect_key'] );
+		if ( isset( $_POST['aaf_rapidconnect_auth_url'] ) ) yourls_update_option( 'aaf_rapidconnect_auth_url', $_POST['aaf_rapidconnect_auth_url'] );
+		if ( isset( $_POST['aaf_rapidconnect_issuer'] ) ) yourls_update_option( 'aaf_rapidconnect_issuer', $_POST['aaf_rapidconnect_issuer'] );
+		if ( isset( $_POST['aaf_rapidconnect_audience'] ) ) yourls_update_option( 'aaf_rapidconnect_audience', $_POST['aaf_rapidconnect_audience'] );
+		if ( isset( $_POST['aaf_attribute_user'] ) ) yourls_update_option( 'aaf_attribute_user', $_POST['aaf_attribute_user'] );
+	}
+
+
+	// Get values from database
+	$aaf_rapidconnect_key = yourls_get_option( 'aaf_rapidconnect_key' );
+	$aaf_rapidconnect_auth_url = yourls_get_option( 'aaf_rapidconnect_auth_url' );
+	$aaf_rapidconnect_issuer = yourls_get_option( 'aaf_rapidconnect_issuer' );
+	$aaf_rapidconnect_audience = yourls_get_option( 'aaf_rapidconnect_audience' );
+	$aaf_attribute_user = yourls_get_option( 'aaf_attribute_user' );
+
+	$nonce = yourls_create_nonce( 'aaf_rapidconnect' );
+	yourls_debug_log( "aaf_rapidconnect_do_page() nonce == " . $nonce );
+
+	// heredoc to the form
+	echo <<<HTML
+	<div id="wrap">
+		<h2>AAF Rapid Connect</h2>
+		<form method="post" id="aaf_rapidconnect_form" name="aaf_rapidconnect_form">
+			<input type="hidden" name="nonce" value="$nonce" />
+
+			<div>
+				<input type="hidden" name="aaf_rapidconnect_key" value="$aaf_rapidconnect_key" />
+				<label for="aaf_rapidconnect_key" form="aaf_rapidconnect_form">Key:</label>
+				<input type="text" value="$aaf_rapidconnect_key" id="aaf_rapidconnect_key" name="aaf_rapidconnect_key" size="32" />
+			</div>
+
+			<div>
+				<input type="hidden" name="aaf_rapidconnect_issuer" value="$aaf_rapidconnect_issuer" />
+				<label for="aaf_rapidconnect_issuer" form="aaf_rapidconnect_form">Issuer:</label>
+				<input type="text" value="$aaf_rapidconnect_issuer" id="aaf_rapidconnect_issuer" name="aaf_rapidconnect_issuer" size="32" />
+			</div>
+
+			<div>
+				<input type="hidden" name="aaf_rapidconnect_audience" value="$aaf_rapidconnect_audience" />
+				<label for="aaf_rapidconnect_audience" form="aaf_rapidconnect_form">Audience (URL of the instance):</label>
+				<input type="text" value="$aaf_rapidconnect_audience" id="aaf_rapidconnect_audience" name="aaf_rapidconnect_audience" size="32" />
+			</div>
+
+			<div>
+				<input type="hidden" name="aaf_rapidconnect_auth_url" value="$aaf_rapidconnect_auth_url" />
+				<label for="aaf_rapidconnect_auth_url" form="aaf_rapidconnect_form">Auth URL:</label>
+				<input type="text" value="$aaf_rapidconnect_auth_url" id="aaf_rapidconnect_auth_url" name="aaf_rapidconnect_auth_url" size="72" />
+			</div>
+
+			<div>
+				<input type="hidden" name="aaf_attribute_user" value="$aaf_attribute_user" />
+				<label for="aaf_attribute_user" form="aaf_attribute_user">User attribute:</label>
+				<input type="text" value="$aaf_attribute_user" id="aaf_attribute_user" name="aaf_attribute_user" size="32" />
+			</div>
+
+			<div>
+				<input type="submit" value="Submit" />
+			</div>
+		</form>
+	</div>
+HTML;
+}
+
 ?>
